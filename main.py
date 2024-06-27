95a6e009b993a71b581f2b38e997d20e808e1e3e
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-06-27 11:39:00 +0800

    separated the coordinate maps of each dimensions.

--- main.py
+++ main.py
@@ -2,28 +2,42 @@ import numpy as np
 import time
 from PIL import Image
 
-# path where the player_coordinates.txt file is located.  Its either in `cmap/run` or where this mod's `.jar` file is located.
-path = "player_coordinates.txt"
+overworld_path = r"C:\Users\yuane\AppData\Roaming\.minecraft\minecraft_overworld_player_coordinates.txt"
+nether_path = r"C:\Users\yuane\AppData\Roaming\.minecraft\minecraft_the_nether_player_coordinates.txt"
+end_path = r"C:\Users\yuane\AppData\Roaming\.minecraft\minecraft_the_end_player_coordinates.txt"
 
-def generate_image(coordinates, resolution):
+resolution = 32768
+
+def generate_image(file_name, coordinates, resolution):
     image = np.ones((resolution, resolution, 3), dtype=np.uint8) * 255
     offset = resolution // 2
     image[coordinates[:, 0] + offset, coordinates[:, 1] + offset] = [0, 0, 0]
 
     image = Image.fromarray(image)
-    image.save("map.png")
+    image.save(f"{file_name}.png")
+
+def get_coordinates(file_path):
+    coordinates = [(0, 0)]
+    with open(file_path, "r") as file:
+        if file.readline() == "": return np.array(coordinates)
+        for line in file:
+            x, z = map(float, line.strip().split(", "))
+            coordinates.append((round(x), round(z)))
+    return np.array(coordinates)
 
 start_time = time.time()
-coordinates = [(0, 0)]
-with open(path) as file:
-    for line in file:
-        x, y = map(float, line.strip().split(", "))
-        coordinates.append((round(x), round(y)))
 
-minecraft_coordinates = np.array(coordinates)
-resolution = 32768
+overworld_coordinates = get_coordinates(overworld_path)
+generate_image("overworld", overworld_coordinates, resolution)
+overworld_coordinates = None
 
-generate_image(minecraft_coordinates, resolution)
+nether_coordinates = get_coordinates(nether_path)
+generate_image("nether", nether_coordinates, resolution)
+nether_coordinates = None
+
+end_coordinates = get_coordinates(end_path)
+generate_image("end", end_coordinates, resolution)
+end_coordinates = None
 
 end_time = time.time()
 print("Time taken: {:.2f} seconds".format(end_time - start_time))
